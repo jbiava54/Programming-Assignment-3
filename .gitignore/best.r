595607fc5c8717fa139c1018651dffefeca4ff76
Jonathan <jonathan.biava@gmail.com>
2017-07-13 12:02:14 -0400

    Update best.r

--- .gitignore/best.r
+++ .gitignore/best.r
@@ -2,11 +2,11 @@ best <- function(state, outcome) {
         ## read outcome data
         outcome_measures <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         data_df <- as.data.frame(cbind(outcome_measures[,2],  ## hospital
-                                          outcome_measures[,7],  ## state
-                                          outcome_measures[,11], ## heart attack
-                                          outcome_measures[,17], ## heart failure 
-                                          outcome_measures[,23], ## pneumonia
-                                        stringsasFactors = FALSE, na.strings = "Not available"))
+                                       outcome_measures[,7],  ## state
+                                       outcome_measures[,11], ## heart attack
+                                       outcome_measures[,17], ## heart failure 
+                                       outcome_measures[,23], ## pneumonia
+                                       stringsasFactors = FALSE, na.strings = "Not Available"))
         data_df[,3] <- as.numeric(data_df[,3])
         data_df[,4] <- as.numeric(data_df[,4])
         data_df[,5] <- as.numeric(data_df[,5])
@@ -15,26 +15,24 @@ best <- function(state, outcome) {
         ## state
         states <- as.character(data_df$state)
         outcomes <- c("heart attack", "heart failure", "pneumonia")
-
+        
         if((state %in% states) == FALSE) {
                 stop(print("Invalid State"))
-                                
+                
         }
-       else if((outcome %in% outcomes) == FALSE) {
+        else if((outcome %in% outcomes) == FALSE) {
                 stop(print("Invalid Outcome"))
-       }
-       else {
-                        state_subset <- data_df[data_df[,state] == state,]
-                        outcome_subset <- state_subset[, outcome]
-                        calcmin <- min(outcome_subset, na.rm = TRUE) 
-                        minrow <- which(outcome_subset == calcmin)
-       }
-   
-        hosp_name <- data_df[minrow,1]        
-        return(hosp_name)
-        ## create a function that finds the min of when you input a state and outcome
-        
-        
+        }
+        else {
+                state_subset <- data_df[data_df[,2] == state,]
+                outcome_subset <- state_subset[, outcome]
+                calcmin <- min(outcome_subset, na.rm = TRUE) 
+                minrow <- which(outcome_subset == calcmin)
+                hosp_name <- as.character(state_subset[minrow,1])
+                return(hosp_name)
+        }
+
+}
         
         
         ## ties : sorted alphabetically 
@@ -44,4 +42,4 @@ best <- function(state, outcome) {
         
         ## return hospital name in that state with lowest 30 day death rat      
      
-        }
+       
